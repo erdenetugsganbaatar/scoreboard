da58743ec7415cd9ac9ae5f3681035d75af43d49
erdenee <erdenetugsganbaatar@gmail.com>
2021-09-09 18:12:48 +0800

    added state

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    Hello Vue World!
+    <Start v-if="!isStarted" :startMatch="startMatch"></Start>
+    <Match v-else/>
   </div>
 </template>
 <script>
+import Start from "./views/Start.vue";
+import Match from "./views/Match.vue";
 
 export default {
-  name: "App",
-  data() {
+  name:"App",
+  data(){
     return {
-      
-    };
+      isStarted: false
+    }
+  },
+  components: {
+    Start,
+    Match
   },
   methods: {
- 
-  },
-};
+    
+  }
+}
 </script>
-
 <style >
+  
 </style>
